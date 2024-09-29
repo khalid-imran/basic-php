d28bda663d878fad3d8047db555b9788bd6bb23e
Khalid Imran <ekhalid090@gmail.com>
2024-09-29 19:54:49 +0600

    route done

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 require "functions.php";
-
-$url = parse_url($_SERVER["REQUEST_URI"])["path"];
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if  (array_key_exists($url, $routes)) {
-    require $routes[$url];
-}
+require "routes.php";
